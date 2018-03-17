b7486de658e8714574ee9843c69e43fc55bde69e
Shade5 <f2013112@goa.bits-pilani.ac.in>
2018-03-17 12:19:41 +0530

    d

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 $( function() {
 
 	$('.tiles').on('mouseover', function() {
-		$( this ).hide(1000);
+		$( this ).find("img").animate({
+			height: "350px",
+			width: "auto",
+			opacity:0.7,
+			},
+			400);
+		$( this ).find(".text").fadeIn(400);
 	});
+
+	$('.tiles').on('mouseout', function() {
+		$( this ).find("img").animate({
+			height: "300px",
+			width: "auto",
+			opacity:1
+			},
+			400)
+		$( this ).find(".text").fadeOut(400);
+	});
+
 
 });
