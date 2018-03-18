cdb00587985e766eb8be8ec85736b7836d1f2834
Shade5 <f2013112@goa.bits-pilani.ac.in>
2018-03-18 14:38:52 +0530

    e

--- js/script.js
+++ js/script.js
@@ -2,23 +2,31 @@ $( function() {
 
 	$('.tiles').on('mouseover', function() {
 		$( this ).find("img").animate({
-			height: "350px",
-			width: "auto",
 			opacity:0.7,
 			},
 			400);
-		$( this ).find(".text").fadeIn(400);
+		$( this ).find(".middle").fadeIn(400);
 	});
 
 	$('.tiles').on('mouseout', function() {
 		$( this ).find("img").animate({
-			height: "300px",
-			width: "auto",
 			opacity:1
 			},
-			400)
-		$( this ).find(".text").fadeOut(400);
+			400);
+		$( this ).find(".middle").fadeOut(400);
 	});
 
-
+	$('.options').on('click', function() {
+		var $chil = $( this )
+		var $sib = $( this ).siblings();
+		$sib.each(function() {
+			$(this).removeClass('active')
+			$(this).addClass('inactive')
+			$($(this).attr('data-optionsid')).slideUp(500, function(){
+				$($chil.attr('data-optionsid')).slideDown(500);
+				$chil.addClass('active')
+				$chil.removeClass('inactive')
+			});
+		});
+	});
 });
